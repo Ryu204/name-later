c1208d33f458f6adbe3960bc7d28817731dababc
Ryu204 <nguyenanhbao2356@gmail.com>
2024-11-28 18:46:38 +0700

    Chores: improve constants file format

--- Scripts/constants.gd
+++ Scripts/constants.gd
@@ -1,25 +1,23 @@
 class_name Constants
 
-const _COMPONENT_PREFIX = 'res://Components/%s'
-const SCENE_SHAPE = _COMPONENT_PREFIX % 'shape.tscn'
-const SCENE_PLAYER = _COMPONENT_PREFIX % 'Player/player.tscn'
-const SCENE_CAR = _COMPONENT_PREFIX % 'Enemies/car.tscn'
-const SCENE_CAR_DRUNK = _COMPONENT_PREFIX % 'Enemies/car_drunk.tscn'
-const SCENE_BIKE = _COMPONENT_PREFIX % 'Enemies/bike.tscn'
-const SCENE_BIKE_PRO = _COMPONENT_PREFIX % 'Enemies/bike_pro.tscn'
-const SCENE_DRONE = _COMPONENT_PREFIX % 'Enemies/drone.tscn'
-const SCENE_MOTHERSHIP = _COMPONENT_PREFIX % 'Enemies/mothership.tscn'
-const SCENE_TANK = _COMPONENT_PREFIX % 'Enemies/tank.tscn'
-const SCENE_TANK_PRO = _COMPONENT_PREFIX % 'Enemies/tank_pro.tscn'
-
-const SCENE_ROCK = _COMPONENT_PREFIX % 'Environments/rock.tscn'
-const SCENE_TREE = _COMPONENT_PREFIX % 'Environments/tree.tscn'
-const SCENE_MUD = _COMPONENT_PREFIX % 'Environments/mud.tscn'
-const SCENE_ICE = _COMPONENT_PREFIX % 'Environments/ice.tscn'
-
-const SCENE_EXPLOSION = _COMPONENT_PREFIX % 'explosion.tscn'
-
 const GAME_OBJECT_LINE_WIDTH = 2.0
 const GAME_OBJECT_ROCK_MAX_SPEED = 0.2
-
 const EPSILON = 1e-5
+
+const _COMPONENT_PREFIX         = 'res://Components/%s.tscn'
+const SCENE_SHAPE               = _COMPONENT_PREFIX % 'shape'
+const SCENE_PLAYER              = _COMPONENT_PREFIX % 'Player/player'
+const SCENE_CAR                 = _COMPONENT_PREFIX % 'Enemies/car'
+const SCENE_CAR_DRUNK           = _COMPONENT_PREFIX % 'Enemies/car_drunk'
+const SCENE_BIKE                = _COMPONENT_PREFIX % 'Enemies/bike'
+const SCENE_BIKE_PRO            = _COMPONENT_PREFIX % 'Enemies/bike_pro'
+const SCENE_DRONE               = _COMPONENT_PREFIX % 'Enemies/drone'
+const SCENE_MOTHERSHIP          = _COMPONENT_PREFIX % 'Enemies/mothership'
+const SCENE_TANK                = _COMPONENT_PREFIX % 'Enemies/tank'
+const SCENE_TANK_PRO            = _COMPONENT_PREFIX % 'Enemies/tank_pro'
+const SCENE_ROCK                = _COMPONENT_PREFIX % 'Environments/rock'
+const SCENE_TREE                = _COMPONENT_PREFIX % 'Environments/tree'
+const SCENE_MUD                 = _COMPONENT_PREFIX % 'Environments/mud'
+const SCENE_ICE                 = _COMPONENT_PREFIX % 'Environments/ice'
+const SCENE_EXPLOSION           = _COMPONENT_PREFIX % 'explosion'
+const SCENE_SPEED_UP            = _COMPONENT_PREFIX % 'Powerups/speed_up'
