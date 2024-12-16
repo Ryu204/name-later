bf791a728c5e172d215526516d87f3b521c2285d
Ryu204 <nguyenanhbao2356@gmail.com>
2024-12-16 16:59:31 +0700

    Feat: mud and ice now ease in

--- Components/Environments/mud.gd
+++ Components/Environments/mud.gd
@@ -5,6 +5,7 @@ extends Area2D
 @export_group('Appearance')
 @export var color: Color
 @export var radius = 100.0
+@export var ease_in_seconds = 4.0
 @export_group('Stats')
 @export var damping_ratio = 0.7
 @export var turn_strength_ratio = 1.0
@@ -14,10 +15,22 @@ var _cirlce_shape: CircleShape2D:
 	get:
 		return _collision_shape.shape as CircleShape2D
 
+var _color_tween: Tween
+
 func _ready() -> void:
 	_cirlce_shape.radius = radius
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
+	_init_color_tween()
+
+func _init_color_tween() -> void:
+	_color_tween = create_tween()
+	var original_alpha = color.a
+	color.a = 0
+	_color_tween.tween_property(self, "color:a", original_alpha, 3).set_trans(Tween.TRANS_SINE)
+
+func _process(_delta: float) -> void:
+	queue_redraw()
 
 func _draw() -> void:
 	draw_circle(Vector2.ZERO, radius, color, true, -1.0, true)
